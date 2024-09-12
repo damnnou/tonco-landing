867e85e6c09f4dde57cfcfd7d0e6dd55c6b3cd3c
lilchizh <xlilchizhx@gmail.com>
2024-09-12 21:30:14 +0700

    add Next config

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+};
+
+export default nextConfig;
