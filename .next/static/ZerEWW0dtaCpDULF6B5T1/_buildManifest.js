bce5c85a00ad60151ea2aa53033f4b5026ed17e4
damnnou <skdamn@outlook.com>
2024-09-12 17:07:15 +0300

    add build

---
+++ .next/static/ZerEWW0dtaCpDULF6B5T1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/829-09b0144d401eafc1.js","static/chunks/pages/index-b45c1e0f1414f96f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
